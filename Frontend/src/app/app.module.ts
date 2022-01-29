0b849c8d9dde221d47271623c4224dd97cae5f96
arijitd97 <darijit2904@gmail.com>
2022-01-29 16:37:49 -0500

    Added navigation bar
    Navigation bar contains Home, Register and Login buttons

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,19 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatSliderModule } from '@angular/material/slider'
+import { MatSliderModule } from '@angular/material/slider';
+import { NavBarComponent } from './nav-bar/nav-bar.component'
+
+import { AngularMaterialModule } from  './material.module';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { LogInComponent } from './log-in/log-in.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LogInComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    AngularMaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
