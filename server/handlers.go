4889e9376b9703e7487afb66de199c41bad0514b
vishesha <39860389+vishesha@users.noreply.github.com>
2022-03-02 14:46:06 -0500

    jwt _id to email_modif

--- server/handlers.go
+++ server/handlers.go
@@ -4,7 +4,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -194,4 +197,24 @@ func DeleteGuideProfile(c *gin.Context) {
 	d := DB.Where("email = ?", email).Delete(&guideprofile)
 	fmt.Println(d)
 	c.JSON(200, gin.H{"profile with Email #" + email: " deleted"})
+}
+
+func upload(c *gin.Context) {
+	file, header, err := c.Request.FormFile("file")
+	if err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("file err : %s", err.Error()))
+		return
+	}
+	filename := header.Filename
+	out, err := os.Create("public/" + filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+	_, err = io.Copy(out, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filepath := "http://localhost:8080/file/" + filename
+	c.JSON(http.StatusOK, gin.H{"filepath": filepath})
 }
