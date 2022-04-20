8d0067f071a6fc8b244ff2c284c1260aea8b6097
harshitha0422 <31062202+harshitha0422@users.noreply.github.com>
2022-04-20 17:52:15 -0400

    Necessary UI changes have been made

--- Frontend/cypress/integration/login_spec.ts
+++ Frontend/cypress/integration/login_spec.ts
@@ -16,11 +16,11 @@ describe('login', () => {
   it('Should not login if the password is less than 6 characters', () => {
     cy.visit('/login')
     cy.url().should('include','login');
-    cy.get('[name="email"]').type('aaa@gmail.com');
+    cy.get('[name="email"]').type('xia@gmail.com');
     cy.wait(2000)
-    cy.get('[name="password"]').type('12345');
+    cy.get('[name="password"]').type('password123');
     cy.wait(2000)
-    cy.get('#mat-select-0').click().get('mat-option').contains('Tourist').click();
+    cy.get('#mat-select-0').click().get('mat-option').contains('Guide').click();
     cy.get('button').click();
     cy.wait(2000)
     cy.url().should('not.include', 'home-page');
